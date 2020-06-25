f2c5fdac96535022b1e0327c7f2c64185d1f0118
daniel_subroto <c14170034@john.petra.ac.id>
2020-06-25 14:10:08 +0700

    Update round dependencies and schema

--- Round/schemas.py
+++ Round/schemas.py
@@ -10,3 +10,34 @@ class RoundSchema(Schema):
     num_civilian = fields.Int(required=True)
     num_undercover = fields.Int(required=True)
     status = fields.Int(required=True)
+
+class RoundDetailSchema(Schema):
+    id = fields.Int(required=True)
+    id_round = fields.Int(required=True)
+    id_user = fields.Int(required=True)
+    id_role = fields.Int(required=True)
+    condition = fields.Str(required=True)
+    status = fields.Int(required=True)
+
+class TurnDetailSchema(Schema):
+    id = fields.Int(required=True)
+    id_round_detail = fields.Int(required=True)
+    turn = fields.Int(required=True)
+    user_word = fields.Str(required=True)
+    user_desc = fields.Str(required=True)
+    status = fields.Int(required=True)
+
+class ResultRoundSchema(Schema):
+    status = fields.Int(required=True)
+    msg = fields.Str(required=True)
+    data = fields.List(fields.Nested(RoundSchema))
+
+class ResultRoundDetailSchema(Schema):
+    status = fields.Int(required=True)
+    msg = fields.Str(required=True)
+    data = fields.List(fields.Nested(RoundDetailSchema))
+
+class ResultTurnDetailSchema(Schema):
+    status = fields.Int(required=True)
+    msg = fields.Str(required=True)
+    data = fields.List(fields.Nested(TurnDetailSchema))
